6dcf89cbab36fa91edffd90d604fb7126be7bb26
Lahiru Samishka <lahirusamishka@gmail.com>
2023-10-09 08:52:05 +0530

    Add Api to crete customer

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('customer', function(Request $request) {
+    return Customer::create($request->all());
 });
